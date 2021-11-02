8a0fe13a413c501705f71da4da3c1254756e34cc
Matt <87787075+maty714@users.noreply.github.com>
2021-11-01 23:26:43 -0400

    Create main.js

---
+++ Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,14 @@
+function myFunction(){
+
+ var str = "Hello World";
+ var result = str.fontcolor("Green");
+
+ document.getElementById("Function").innerHTML = result;
+
+}
+
+function concat() {
+    var str = "Hello";
+    str += "world";
+    document.getElementById("demo").innerHTML = str;
+    }
