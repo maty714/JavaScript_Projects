53f9d932e4e83577b49655baccf5c1bf99da27c7
Matt <87787075+maty714@users.noreply.github.com>
2021-11-01 20:44:17 -0400

    Update main.js

--- Projects/Project1_expressions_alert/JS/main.js
+++ Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,14 @@
 
-var x = "Hello World";
-document.write(x);
+var w = " and goodbye"
+var M = "Matt said \"Hello World \"";
+var y = '1', z = '2';
+var x = M.fontcolor("Blue");
+var y = y.fontcolor("Red");
+var z = z.fontcolor("Green");
+var num1 = 10, num2 = 20;
+
+window.alert(M);
+document.write(y + " ");
+document.write(z + " ");
+document.write(x + w + " ");
+document.write(num1 + num2)
