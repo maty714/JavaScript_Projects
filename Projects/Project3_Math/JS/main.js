2eaa69f414dac8456917cd56060fd1d5f7d8f60d
Matt <87787075+maty714@users.noreply.github.com>
2021-11-04 19:21:21 -0400

    Create main.js

---
+++ Projects/Project3_Math/JS/main.js
@@ -0,0 +1,15 @@
+function add(){
+    var num1 = 20;
+    var num2 = 15;
+
+    document.getElementById("Math").innerHTML = num1 + num2;
+}
+
+function subtract(){
+    var num1 = 20;
+    var num2 = 15;
+
+    document.getElementById("subtract").innerHTML = num1 - num2;
+
+}
+
