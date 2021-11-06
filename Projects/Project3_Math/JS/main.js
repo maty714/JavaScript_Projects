7a4ad95b389a88c3be7b0d6cd2b059872ccec8d0
Matt <87787075+maty714@users.noreply.github.com>
2021-11-06 15:19:23 -0400

    Update main.js

--- Projects/Project3_Math/JS/main.js
+++ Projects/Project3_Math/JS/main.js
@@ -2,14 +2,32 @@ function add(){
     var num1 = 20;
     var num2 = 15;
 
-    document.getElementById("Math").innerHTML = num1 + num2;
+    document.getElementById("Math").innerHTML = num1 * num2;
 }
 
 function subtract(){
     var num1 = 20;
     var num2 = 15;
 
-    document.getElementById("subtract").innerHTML = num1 - num2;
+    document.getElementById("subtract").innerHTML = num1 / num2;
 
 }
 
+function many(){
+    var num1 = 15;
+    var num2 = 20;
+document.getElementById("many").innerHTML = -((num1/num2)+200);
+}
+
+function increment(){
+    var x = 10;
+    
+    x++;
+    document.getElementById("x").innerHTML = x;
+    
+}
+
+function random(){
+    x =  Math.floor(Math.random() * 100);
+    document.getElementById("random").innerHTML = x;
+}
