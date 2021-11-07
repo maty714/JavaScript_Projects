010c95570ae51e332e0cdd575af357d24e451af1
Matt <87787075+maty714@users.noreply.github.com>
2021-11-07 17:28:44 -0500

    Update main.js

---
+++ Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,41 @@
+document.write("10"+ " " +5);
+document.write("<br>");
+document.write(typeof "word");
+document.write("<br>");
+document.write(2E310);
+document.write("<br>");
+document.write(10>2);
+document.write("<br>");
+document.write(1>2);
+document.write("<br>");
+console.log(2+2); //outputs 4 in the browser console
+console.log(2>5);
+document.write("<br>");
+function NAN(){
+
+    document.getElementById("num").innerHTML = 0/0;
+
+}
+
+document.write("<br>");
+function isn(){
+
+    document.getElementById("tf").innerHTML = isNaN('word');
+
+}
+
+document.write("<br>");
+
+var x = '10';
+var y = 10;
+
+document.write(x===y); //triple equal sign checks the whether the two conditions are of the same type and equal. This will output false as x is a string and y is a int
+
+document.write("<br>");
+
+document.write(10>5 || 5>6);
+document.write("<br>");
+document.write(!(10<5)); //not operator
+
+//note to self, operators are the same as C++
+
