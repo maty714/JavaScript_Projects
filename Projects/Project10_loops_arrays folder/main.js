c0bce11dbda63aca31a2fd503b8cb882a1083f4d
Matt <87787075+maty714@users.noreply.github.com>
2021-11-13 19:56:05 -0500

    Update main.js

--- Projects/Project10_loops_arrays folder/main.js
+++ Projects/Project10_loops_arrays folder/main.js
@@ -9,43 +9,56 @@ function while_loop(){
     i++;
     }
 
-  document.getElementById("while").innerhtml = output;
-}
-
-//Function 2 from step 198 of javascript course
-function count_to_ten(){
-  var Digit = "";
-  var X = 1;
-  while(X < 11){
-    Digit += "<br>" + X;
-    X++;
-    }
-
-document.getElementById("Counting_to_Ten").innerHTML = Digit;
-
+  document.getElementById("while").innerHTML = output;
 }
 
 
 
+function for_loop(){
+  var output = "";
+  var i;
+  for (i = 0; i < 10; i++){
+
+    output += "<br>" + i;
+
+
+  }
+
+  document.getElementById("for").innerHTML = output;
+}
+
+
+
+function myArray(){
+  output = "";
+  var arr = [1,2,3,4,5,6,7,8,9,10];
+  var i;
+
+  for (i = 0; i < arr.length; i++){
+
+    output += "<br>" + arr[i];
+
+  }
+
+  document.getElementById("array").innerHTML = output;
+}
 
 
 
 
+let house = {
 
-//let house = {
+Levels: "3",
+location: "Washington D.C.",
+Color: "White",
+Has_Garage: "Yes",
+description: function() {
+  return this.Color //+ ", has " + this.Levels + " levels, and is located in " + this.location;
+  }
 
-//Levels: "3",
-//location: "Washington D.C.",
-//Color: "White",
-//Has_Garage: "Yes",
-//description: function() {
-//  return this.Color //+ ", has " + this.Levels + " levels, and is located in " + this.location;
-//  }
-///
 
-//};
-
-  
-//document.getElementById("desc").innerHTML = house.description();
+};
+ 
+document.getElementById("desc").innerHTML = house.description();
 
 
