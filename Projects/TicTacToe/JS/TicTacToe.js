9e14d96b5d6adb3a68908a19348b32cf54e2f115
Matt <87787075+maty714@users.noreply.github.com>
2021-11-23 20:18:31 -0500

    Updated Tic Tac Toe

--- Projects/TicTacToe/JS/TicTacToe.js
+++ Projects/TicTacToe/JS/TicTacToe.js
@@ -10,56 +10,55 @@ function placeXorO(squareNumber){
         let select = document.getElementById(squareNumber);
         
         if (activePlayer ==='X'){
-            select.style.backgroundImage = 'url("./Images/x.png")';
+            select.style.backgroundImage = 'url("./Images/x_new.png")';
         }
         else {
-            select.style.backgroundImage = 'url("./Images/o.png")';
+            select.style.backgroundImage = 'url("./Images/o_new.png")';
         }
+        
+        selectedSquares.push(squareNumber + activePlayer);
+
+        checkWinConditions();
+
+        if (activePlayer === 'X'){
+            activePlayer = 'O';
+        }
+        else {
+            activePlayer = 'X';
+        }
+        audio('./Media/place.mp3');
+       
+        if (activePlayer === 'O'){
+            /*This function disables clicking for computer choice */
+            disableClick();
+         
+            /*This function waits 1 second before computer places image and enables click */
+            setTimeout(function(){computersTurn(); }, 1000)
+        }
+        
+        /*Returning True is needed for the computersTurn() to work */
+        return true;
+        
     }
-
-    selectedSquares.push(squareNumber + activePlayer);
-
-    checkWinConditions();
+    function computersTurn(){
     
-    if (activePlayer === 'X'){
-
-        activePlayer = 'O';
-    }
-    else{
-        activePlayer = 'X';
-    }
-
-    audio('./Media/place.mp3');/*check this*/
+        let success = false;
+        let pickASquare;
     
+        while(!success){
     
-    if (activePlayer === 'O'){
-        /*This function disables clicking for computer choice */
-        disableClick();
-     
-        /*This function waits 1 second before computer places image and enables click */
-        setTimeout(function(){computersTurn(); }, 1000)
-    }
+            pickASquare = String(Math.floor(Math.random() * 9));
     
-    /*Returning True is needed for the computersTurn() to work */
-    return true;
-}
-
-function computersTurn(){
+            if (placeXorO(pickASquare)){
     
-    let success = false;
-    let pickASquare;
-
-    while(!success){
-
-        pickASquare = String(Math.floor(Math.random() * 9));
-
-        if (placeXorO(pickASquare)){
-
-            placeXorO(pickASquare);
-            success= true;
+                placeXorO(pickASquare);
+                success= true;
+            };
         }
     }
 }
+
+
 
 function checkWinConditions(){
 
@@ -113,8 +112,9 @@ function checkWinConditions(){
     }
     else if (selectedSquares.length >= 9){
         audio('./Media/tie.mp3')
-    }
+    
     setTimeout(function(){ resetGame(); }, 1000)
+    }
 }
 
 function arrayIncludes(squareA, squareB, squareC){
@@ -132,7 +132,8 @@ function arrayIncludes(squareA, squareB, squareC){
 
 function disableClick(){
 
-    body.style.pointerEvents = 'none';
+    
+    body.style.pointerEvents = 'none'
     setTimeout(function () {body.style.pointerEvents = 'auto';} ,1000);
 }
 
@@ -143,5 +144,85 @@ function audio(audioURL) {
 
 }
 
+function drawWinLine(coordX1, coordY1, coordX2, coordY2){
+
+    const canvas = document.getElementById('win-lines')
+    const c = canvas.getContext('2d');
+
+    let x1 = coordX1, 
+        y1 = coordY1,
+        x2 = coordX2,
+        y2 = coordY2,
+        x = x1,
+        y= y1;
+
+
+    function animateLineDrawing(){
+        
+        const animationLoop = requestAnimationFrame(animateLineDrawing)
+        c.clearRect(0,0,608,608)
+        c.beginPath();
+        c.moveTo(x1,y1)
+        c.lineTo(x,y)
+        c.lineWidth = 10;
+        c.strokeStyle = 'rgba(70, 255, 33, .8)';
+        c.stroke();
+
+        if(x1 <= x2 && y1 <= y2){
+            
+            if(x < x2)
+            {
+                x += 10;
+            }
+            if(y < y2){
+
+                y += 10;
+            }
+            if(x >= x2 && y >= y2){
+ 
+                cancelAnimationFrame(animationLoop);
+            }
+        }
+        if(x1 <= x2 && y1 >= y2){
+            if (x < x2){
+
+                x += 10;
+            }
+            if(y > y2){
+
+                y -=10;
+            }
+            if(x >= x2 && y <= y2) {
+            
+                cancelAnimationFrame(animationLoop);
+            }
+
+        }
+
+    }
+
+    function clear(){
+
+        const animationLoop = requestAnimationFrame(clear);
+        c.clearRect(0,0,608,608)
+        cancelAnimationFrame(animationLoop)
+    }
+
+    disableClick();
+    audio('./media/winGame.mp3');
+    animateLineDrawing();
+    setTimeout(function() { clear(); resetGame(); }, 1000);
+}
+
+function resetGame(){
+
+    for (let i = 0; i <9; i++){
+
+        let square = document.getElementById(String(i))
+        square.style.backgroundImage = '';
+
+    }
+    selectedSquares = [];
+}
 
 
